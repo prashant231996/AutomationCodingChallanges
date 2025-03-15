610b557f0c022383244083af8f6168028436d881
prashant231996 <more23101996@gmail.com>
2025-03-15 22:50:17 +0530

    Added cof=ding challange 15/03/25

--- src/test/java/com/javaProgram/codingChallange/CountOfEachCharacterAndReplaceItsDuplicate.java
+++ src/test/java/com/javaProgram/codingChallange/CountOfEachCharacterAndReplaceItsDuplicate.java
@@ -1,5 +1,7 @@
 package com.javaProgram.codingChallange;
 
+import java.util.HashMap;
+
 public class CountOfEachCharacterAndReplaceItsDuplicate {
 
 	public static void main(String[] args) {
@@ -10,8 +12,33 @@ public class CountOfEachCharacterAndReplaceItsDuplicate {
             Output= "2u22m22i2n"
 		 */
 		
-		
-
-	}
+		String str="Automation";
+		String result="";
+		str=str.toLowerCase();
+		HashMap<Character,Integer>hmap=new HashMap<Character, Integer>();
+        for(Character ch:str.toCharArray())
+        {
+        	if(hmap.containsKey(ch))
+        	{
+        		hmap.put(ch, hmap.get(ch)+1);
+        	}
+        	else
+        	{
+        		hmap.put(ch,1);
+        	}
+        }
+        for(Character ch:str.toCharArray())
+        {
+        	if(hmap.get(ch)>1)
+        	{
+        		result=result+String.valueOf(hmap.get(ch));
+        	}
+        	else
+        	{
+        		result=result+ch;
+        	}
+        }
+        System.out.println("Resultant string is "+result);
+	} 
 
 }
---
+++ src/test/java/com/javaProgram/codingChallange/ReverseElementsOfArrayWithoutUsingAdditionalArray.java
@@ -0,0 +1,16 @@
+package com.javaProgram.codingChallange;
+
+public class ReverseElementsOfArrayWithoutUsingAdditionalArray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int array[]= {1,2,3,4,5};
+		for(int i=0;i<array.length;i++)
+		{
+			
+		}
+
+	}
+
+}
